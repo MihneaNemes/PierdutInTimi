ba1c5714935c90c46dd7106e0e288ea99e1e6f22
MihneaNemes <mihneaneme@gmail.com>
2025-10-19 15:17:45 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/guessr_timi/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.guessr_timi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/com/guessr/guessr_timi/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.guessr.guessr_timi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
